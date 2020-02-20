407d55cc16f0b223d3767e03ac23a5c3bfa62ef7
Eddie <eddie@crunchbase.com>
2020-02-19 17:43:51 -0800

    CB-00000 Batch flush correctly for schema

--- src/main/java/com/github/jcustenborder/kafka/connect/archive/Archive.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/archive/Archive.java
@@ -17,6 +17,9 @@ package com.github.jcustenborder.kafka.connect.archive;
 
 import com.github.jcustenborder.kafka.connect.utils.config.Description;
 import com.github.jcustenborder.kafka.connect.utils.config.DocumentationNote;
+import org.apache.kafka.common.cache.SynchronizedCache;
+import org.apache.kafka.common.cache.LRUCache;
+import org.apache.kafka.common.cache.Cache;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Schema;
@@ -27,11 +30,41 @@ import org.apache.kafka.connect.transforms.Transformation;
 import java.util.HashMap;
 import java.util.Map;
 
+
+class SchemaPair {
+  Schema keySchema;
+  Schema valueSchema;
+
+  public SchemaPair(Schema keySchema, Schema valueSchema) {
+    this.keySchema = keySchema;
+    this.valueSchema = valueSchema;
+  }
+
+  public int hashCode() {
+    int result = (this.keySchema != null ? this.keySchema.hashCode() : 0);
+    result = 31 * result + (this.valueSchema != null ? this.valueSchema.hashCode() : 0);
+    return result;
+  }
+
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    } else if (!(o instanceof SchemaPair)) {
+      return false;
+    }
+    SchemaPair other = (SchemaPair) o;
+    return keySchema.equals(other.keySchema) && valueSchema.equals(other.valueSchema);
+  }
+}
+
 @Description("The Archive transformation is used to help preserve all of the data for a message when archived to S3.")
 @DocumentationNote("This transform works by copying the key, value, topic, and timestamp to new record where this is all " +
     "contained in the value of the message. This will allow connectors like Confluent's S3 connector to properly archive " +
     "the record.")
 public class Archive<R extends ConnectRecord<R>> implements Transformation<R> {
+
+  private Cache<SchemaPair, Schema> schemaUpdateCache;
+
   @Override
   public R apply(R r) {
     if (r.valueSchema() == null) {
@@ -41,19 +74,29 @@ public class Archive<R extends ConnectRecord<R>> implements Transformation<R> {
     }
   }
 
+  private Schema makeUpdatedSchema(R r) {
+    return SchemaBuilder.struct()
+            .name("com.github.jcustenborder.kafka.connect.archive.Storage")
+            .field("key", r.keySchema())
+            .field("value", r.valueSchema())
+            .field("topic", Schema.STRING_SCHEMA)
+            .field("timestamp", Schema.INT64_SCHEMA);
+  }
+
   private R applyWithSchema(R r) {
-    final Schema schema = SchemaBuilder.struct()
-        .name("com.github.jcustenborder.kafka.connect.archive.Storage")
-        .field("key", r.keySchema())
-        .field("value", r.valueSchema())
-        .field("topic", Schema.STRING_SCHEMA)
-        .field("timestamp", Schema.INT64_SCHEMA);
+    SchemaPair pair = new SchemaPair(r.keySchema(), r.valueSchema());
+    Schema schema = schemaUpdateCache.get(pair);
+    if (schema == null) {
+      schema = makeUpdatedSchema(r);
+      schemaUpdateCache.put(pair, schema);
+    }
+
     Struct value = new Struct(schema)
         .put("key", r.key())
         .put("value", r.value())
         .put("topic", r.topic())
         .put("timestamp", r.timestamp());
-    return r.newRecord(r.topic(), r.kafkaPartition(), null, null, schema, value, r.timestamp());
+    return r.newRecord(r.topic(), r.kafkaPartition(), r.keySchema(), r.key(), schema, value, r.timestamp());
   }
 
   @SuppressWarnings("unchecked")
@@ -83,6 +126,6 @@ public class Archive<R extends ConnectRecord<R>> implements Transformation<R> {
 
   @Override
   public void configure(Map<String, ?> map) {
-
+    schemaUpdateCache = new SynchronizedCache<>(new LRUCache<SchemaPair, Schema>(16));
   }
 }
